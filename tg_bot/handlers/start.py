914ea5a8380ddacd04cd087c81a9ebb70ead3f9e
gglebbb <gleb@prilepskiy.ru>
2024-02-10 00:25:34 +0300

    from PC

--- tg_bot/handlers/start.py
+++ tg_bot/handlers/start.py
@@ -1,12 +1,21 @@
+from environs import Env
 from aiogram import types, Dispatcher
-from aiogram.dispatcher.filters import Command
+from aiogram.dispatcher.filters import CommandStart
 from aiogram.types import Message
 
+env = Env()
+env.read_env(path=".env")
 
-async def bot_start(message: types.Message):
+
+async def bot_start_with_password(message: types.Message):
     await message.answer("Hello evrybody")
     await message.answer("<tg-spoiler> Try to type /help </tg-spoiler>")
 
 
+async def bot_without_password(messge: types.Message):
+    await messge.answer("You don't know a password(")
+
+
 def register_start(dp: Dispatcher):
-    dp.register_message_handler(callback=bot_start, commands=["start"])
+    dp.register_message_handler(bot_start_with_password, CommandStart(deep_link=env.str("PASSWORD_TO_START")))
+    dp.register_message_handler(bot_without_password, CommandStart())
